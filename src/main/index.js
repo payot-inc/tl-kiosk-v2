a80df44ea4351686ed85b53b1d20f9a31d0b77ec
yonghee <yonghee@yongheeui-MacBookPro.local>
2020-01-13 15:39:19 +0900

    single instance lock

--- src/main/index.js
+++ src/main/index.js
@@ -44,22 +44,21 @@ function createWindow() {
   });
 }
 
-const singleInstanceLock = app.requestSingleInstanceLock();
-if (!singleInstanceLock) {
-  app.quit();
+const gotTheLock = app.requestSingleInstanceLock()
+
+if (!gotTheLock) {
+  app.quit()
   app.exit();
 } else {
-  app.on('second-instance', (event, commendLine, workingDirectory) => {
+  app.on('second-instance', (event, commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
     if (mainWindow) {
-      if (mainWindow.isMinimized()) {
-        mainWindow.restore();
-      } else if (!mainWindow.isVisible()) {
-        mainWindow.show();
-      }
-      mainWindow.focus();
+      if (mainWindow.isMinimized()) mainWindow.restore()
+      mainWindow.focus()
     }
-  });
+  })
 
+  // Create mainWindow, load the rest of the app, etc...
   app.on('ready', createWindow);
 }
 
